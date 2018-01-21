c106efcbdbe0a43c4dd8416c911d14ebb6a51b28
namnv1997 <helloimnvn@gmail.com>
2018-01-21 22:14:59 +0700

    Add files via upload

---
+++ youtube.php
@@ -0,0 +1,141 @@
+<?php
+
+/**
+ * Library Requirements
+ *
+ * 1. Install composer (https://getcomposer.org)
+ * 2. On the command line, change to this directory (api-samples/php)
+ * 3. Require the google/apiclient library
+ *    $ composer require google/apiclient:~2.0
+ */
+require_once 'base/Database.php';
+$base = new Database();
+$db = $base->connnect();
+
+if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+    throw new \Exception('please run "composer require google/apiclient:~2.0" in "' . __DIR__ . '"');
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+session_start();
+
+/*
+ * You can acquire an OAuth 2.0 client ID and client secret from the
+ * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
+ * For more information about using OAuth 2.0 to access Google APIs, please see:
+ * <https://developers.google.com/youtube/v3/guides/authentication>
+ * Please ensure that you have enabled the YouTube Data API for your project.
+ */
+$OAUTH2_CLIENT_ID = '336672653307-b32ode4o7lreqlcnctp43p45keoa8ged.apps.googleusercontent.com';
+$OAUTH2_CLIENT_SECRET = 'aCiABgJ6uttvZVNTPWK5xkg3';
+
+$client = new Google_Client();
+$client->setClientId($OAUTH2_CLIENT_ID);
+$client->setClientSecret($OAUTH2_CLIENT_SECRET);
+$client->setScopes('https://www.googleapis.com/auth/youtube');
+$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+    FILTER_SANITIZE_URL);
+$client->setRedirectUri($redirect);
+
+// Define an object that will be used to make all API requests.
+$youtube = new Google_Service_YouTube($client);
+
+// Check if an auth token exists for the required scopes
+$tokenSessionKey = 'token-' . $client->prepareScopes();
+if (isset($_GET['code'])) {
+    if (strval($_SESSION['state']) !== strval($_GET['state'])) {
+        die('The session state did not match.');
+    }
+
+    $client->authenticate($_GET['code']);
+    $_SESSION[$tokenSessionKey] = $client->getAccessToken();
+    header('Location: ' . $redirect);
+}
+
+
+
+if (isset($_SESSION[$tokenSessionKey])) {
+    $client->setAccessToken($_SESSION[$tokenSessionKey]);
+}
+
+// Check to ensure that the access token was successfully acquired.
+if ($client->getAccessToken()) {
+
+
+    $htmlBody = '';
+    try {
+        // This code subscribes the authenticated user to the specified channel.
+
+        // Identify the resource being subscribed to by specifying its channel ID
+        // and kind.
+
+        $list = array();
+        array_push($list, 'UC0xBCDoQMtaWl1ZZz7WRFVw', 'UCpo83FKgEX5gGEcWZXpSJCw', 'UCqSMvNANwmfY55cwd3ISMMg');
+        foreach ($list as $row){
+
+            $resourceId = new Google_Service_YouTube_ResourceId();
+
+            $resourceId->setChannelId(''.$row.'');
+            $resourceId->setKind('youtube#channel');
+
+            // Create a snippet object and set its resource ID.
+            $subscriptionSnippet = new Google_Service_YouTube_SubscriptionSnippet();
+            $subscriptionSnippet->setResourceId($resourceId);
+
+            // Create a subscription request that contains the snippet object.
+            $subscription = new Google_Service_YouTube_Subscription();
+            $subscription->setSnippet($subscriptionSnippet);
+
+            // Execute the request and return an object containing information
+            // about the new subscription.
+            $subscriptionResponse = $youtube->subscriptions->insert('id,snippet',
+                $subscription, array());
+
+            $htmlBody .= "<h3>Subscription</h3><ul>";
+            $htmlBody .= sprintf('<li>%s (%s)</li>',
+                $subscriptionResponse['snippet']['title'],
+                $subscriptionResponse['id']);
+            $htmlBody .= '</ul>';
+        }
+
+
+    } catch (Google_Service_Exception $e) {
+        $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+            htmlspecialchars($e->getMessage()));
+    } catch (Google_Exception $e) {
+        $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+            htmlspecialchars($e->getMessage()));
+    }
+
+    $_SESSION[$tokenSessionKey] = $client->getAccessToken();
+} elseif ($OAUTH2_CLIENT_ID == 'REPLACE_ME') {
+    $htmlBody = <<<END
+  <h3>Client Credentials Required</h3>
+  <p>
+    You need to set <code>\$OAUTH2_CLIENT_ID</code> and
+    <code>\$OAUTH2_CLIENT_ID</code> before proceeding.
+  <p>
+END;
+} else {
+    // If the user has not authorized the application, start the OAuth 2.0 flow.
+    $state = mt_rand();
+    $client->setState($state);
+    $_SESSION['state'] = $state;
+
+    $authUrl = $client->createAuthUrl();
+    $htmlBody = <<<END
+  <h3>Authorization Required</h3>
+  <p>You need to <a href="$authUrl">authorize access</a> before proceeding.<p>
+END;
+}
+?>
+
+<!doctype html>
+<html>
+<head>
+    <title>Returned Subscription</title>
+</head>
+<body>
+<?= $htmlBody ?>
+</body>
+</html>
